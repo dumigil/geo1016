2b81e017ca5efa47b6e29d036936f9d722710695
Michiel de Jong <m.d.dejong-1@student.tudelft.nl>
2021-02-19 14:19:04 +0100

    Add build

---
+++ cmake-build-debug/3rd_party/glog/CMakeFiles/3rd_glog.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/3rd_party/glog/logging.cc" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/3rd_party/glog/CMakeFiles/3rd_glog.dir/logging.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../3rd_party/glog/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/camera.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/camera.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/clipping_plane.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/clipping_plane.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/constraint.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/constraint.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/drawable.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/drawable.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/drawable_lines.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/drawable_lines.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/drawable_points.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/drawable_points.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/drawable_triangles.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/drawable_triangles.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/frame.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/frame.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/framebuffer_object.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/framebuffer_object.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/key_frame_interpolator.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/key_frame_interpolator.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/manipulated_camera_frame.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/manipulated_camera_frame.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/manipulated_frame.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/manipulated_frame.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/model.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/model.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/opengl_error.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/opengl_error.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/opengl_info.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/opengl_info.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/opengl_timer.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/opengl_timer.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/primitives.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/primitives.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/read_pixel.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/read_pixel.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/renderer.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/renderer.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/setting.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/setting.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/shader_manager.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/shader_manager.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/shader_program.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/shader_program.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/tessellator.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/tessellator.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/texture.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/texture.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/transform.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/transform.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/transform_decompose.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/transform_decompose.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/vertex_array_object.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/vertex_array_object.cpp.o"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/easy3d/viewer/viewer.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/viewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_NO_GLU"
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "."
+  "../3rd_party/glew/include"
+  "../3rd_party/glfw/include"
+  "../3rd_party/glog/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/3rd_party/glew/CMakeFiles/3rd_glew.dir/DependInfo.cmake"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/3rd_party/glfw/src/CMakeFiles/3rd_glfw.dir/DependInfo.cmake"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/core/CMakeFiles/easy3d_core.dir/DependInfo.cmake"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/util/CMakeFiles/easy3d_util.dir/DependInfo.cmake"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/easy3d/fileio/CMakeFiles/easy3d_fileio.dir/DependInfo.cmake"
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1016/A1_Calibration_Code/geo1016/cmake-build-debug/3rd_party/glog/CMakeFiles/3rd_glog.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
